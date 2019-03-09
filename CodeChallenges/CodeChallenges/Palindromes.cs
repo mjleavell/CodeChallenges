d361c010bc2c051d1e32a55b75f67fe25afcbd58
Maggie Leavell <mjleavell@gmail.com>
2019-03-09 11:20:15 -0600

    palindromes challenge complete

---
+++ CodeChallenges/CodeChallenges/Palindromes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeChallenges
+{
+    class Palindromes
+    {
+        //var input = Console.ReadLine();
+
+    }
+}
--- CodeChallenges/CodeChallenges/Program.cs
+++ CodeChallenges/CodeChallenges/Program.cs
@@ -1,12 +1,53 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CodeChallenges
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter a string");
+            var userInput = Console.ReadLine().ToLower();
+            //var inputString = new List<char>();
+            var inputString = "";
+            var compareString = "";
+
+            // Removes punctuation
+            foreach (char myChar in userInput)
+            {
+                if (!char.IsPunctuation(myChar))
+                {
+                    inputString += myChar;
+                }
+            }
+
+            // Removes spaces
+            inputString = inputString.Replace(" ", string.Empty);
+
+            // Reverses the string using the ReverseString method
+            compareString = ReverseString(inputString);
+
+            // Compares the two strings
+            if(compareString == inputString)
+            {
+                Console.WriteLine("True! You have a palindrome");
+            }
+            else
+            {
+                Console.WriteLine("False! You don't have a palindrome");
+            }
+
+            Console.ReadLine();
+        }
+
+        public static string ReverseString(string s)
+        {
+            char[] array = s.ToCharArray();
+            Array.Reverse(array);
+            return new string(array);
         }
     }
 }
+
